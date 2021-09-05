87fe4fa0419a0ef1c6414c343b193f1bebff55e0
Mark Langhamer <langhamerm@gmail.com>
2021-09-05 06:04:23 -0500

    ww

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
